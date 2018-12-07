4528f736985dfb30f1364fe06cd5587ae9532e49
OPPD_PUB_ACCOUNT <OPPD_PUB_ACCOUNT@inspur.com>
2018-12-07 09:02:56 +0800

    项目搭建完成

---
+++ src/test/java/Generator.java
@@ -0,0 +1,34 @@
+/**
+ * FileName: Generator
+ * Author:   shikangshuai
+ * Date:     2018/12/6 20:02
+ * Description:
+ */
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ *
+ * @author shikangshuai
+ * @create 2018/12/6
+ */
+public class Generator {
+    public static void main(String[] args) throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mybatis-generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
